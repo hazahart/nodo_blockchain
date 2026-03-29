091a50df06742186098b681be410eea5efa570d0
Gustavo Ramirez Mireles <gusramireles@hotmail.com>
2026-03-29 01:23:46 -0600

    fix: openapi

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'paths'                => ['api/*'],
-    'allowed_methods'      => ['*'],
-    'allowed_origins'      => ['http://localhost:5173'],
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
-    'allowed_headers'      => ['*'],
-    'exposed_headers'      => [],
-    'max_age'              => 0,
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
     'supports_credentials' => false,
 ];
